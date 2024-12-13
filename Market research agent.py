a084a205eef8642771289cff7318919a89a251fd
Ajay Anand <143182133+AjayAnandx@users.noreply.github.com>
2024-12-13 15:41:13 +0530

    Update Market research agent.py

--- Market research agent.py
+++ Market research agent.py
@@ -1,89 +1,84 @@
 import os 
 from groq import Groq
-from phi.assistant import Assistant
+from phi.agent import Agent
 from phi.tools.duckduckgo import DuckDuckGo
-from phi.tools.tavily import TavilyTools
-from phi.llm.groq import Groq
-from phi.tools.pubmed import PubmedTools
+from phi.tools.searxng import Searxng
+from phi.model.groq import Groq
 from phi.tools.newspaper4k import Newspaper4k
 from phi.tools.apify import ApifyTools
+from phi.tools.arxiv_toolkit import ArxivToolkit
 from rich.pretty import pprint
-from phi.assistant import Assistant, AssistantMemory
-
+from phi.tools.crawl4ai_tools import Crawl4aiTools
+from phi.tools.website import WebsiteTools
+from phi.tools.file import FileTools
 
 # Calling the LLM
-Api_Key = str(input("Enter you Groq_API_Key : "))
-llm = Groq(api_key=Api_Key, model_name="llama-3.1-405b",  )
+llm = Groq(id="llama3-groq-70b-8192-tool-use-preview",api_key="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"  )
 
-
-# Writing all the agents for reasearch marketing
-
-#Query summarizer
-Query_Summarizer = Assistant(
-    Name = "Query Reasearcher Agent",
-    llm = llm,
-    role = "Summarize the User query enter by the query and pass to the agent",
-    Instruction = [
-        "you will receive user queries as input.",
-        "Want to analyze the query to understand its context, key points, and main ideas",
-        "Want to generate a concise summary of the query. The summary should capture the essential information and intent of the query."
-        "Output the summary to the user or the next processing agent."
-    ]
+Query_Splliter = Agent (
+    name  = "query splitter",
+    model = llm,
+    role  = "to split the key details in the query",
+    instructions = ["Split the query into the key point"
+    "1. Url"
+    "2. Topic"
+    "3. content"],
 )
 
-# Market Reasearcher
-Market_researchr = Assistant(
-    Nmae = "Quantum Market_Reasearcher",
-    llm = llm,
-    Role = "Want to Conduct a reasearch on the query give by the user",
-    Instruction = [
-        """Identify key market trends, size, and growth rate.
-           Analyze competitors, including their strengths, weaknesses, pricing strategies, and market positioning."""
+searxng = Searxng(
+    host="http://localhost:53153",
+    engines=["goole"],
+    it=True,
+    fixed_max_results=5,
+    news=True,
+    science=True
+)
+
+Research_Agent = Agent(
+    name = "Research Agent",
+    model = llm,
+    role  = "research the about the user {query}",
+    tools = [Crawl4aiTools(max_length=1000)],
+)
+
+Web_searcher = Agent(
+    name = "web scraper",
+    mnodel = llm,
+    tools = [searxng],
+)
+
+Article_finder = Agent(
+    name = "Article finder",
+    model = llm,
+    role = "To get the paper links from the arvix",
+    tool = [ArxivToolkit(search_arxiv=True)],
+)
+
+Article_reader = Agent(
+    name = "Article reader",
+    model = llm,
+    role = "to read the Arvix paper",
+    tools = [ArxivToolkit(read_arxiv_papers=True)],
+)
+
+Report_Writer  = Agent(
+    name = "Report Writer",
+    model = llm,
+    role = "to write a reoport on the query",
+)
+
+Quantum_Assistant = Agent(
+    name  = "report Genetrator",
+    model = llm,
+    role = "to lead the team of agents",
+    tool = [Report_Writer,Article_reader,Web_searcher,Research_Agent,FileTools,Query_Splliter],
+    instructions = [
+        "Lead the team of agents and guide them to use the correct usage for the user query"
+        "drive the final Report"
+        "Note: if the user pecify a agent only use them"
     ],
-    Tools = [TavilyTools(api_key="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",use_search_context=True),DuckDuckGo()],
-)
-
-# Article Reasearcher
-Article_Reasearcher = Assistant(
-    Name = "Quantum Article Reasearcher",
-    llm = llm,
-    Role = "want to search the artile for Based on the query",
-    Tools = [PubmedTools(max_results=5,email= "ajaysunil35@gmail.com"),Newspaper4k(include_summary=True)],
-    show_tool_calls = True
-)
-
-# Web Scraper
-Web_Scraper = Assistant(
-    Name  = "Quantum Web Scraper",
-    llm = llm,
-    Role = "scrap the web for the content",
-    Instruction = ["""URL(s): [List the URLs to be scraped]
-                   Note: Ensure you have permission to scrape the target websites."""],
-    Tools = [ApifyTools(api_key = "apify_api_Yh9HcZdEiygVOFAmUCheNgqRn7BvI734G9Vm")]
-)
-
-# Content Summarizer
-Content_Summarizer = Assistant(
-    Name  = "Quantum Content Researcher",
-    llm = llm,
-    Role = "want to summarize the content give by the assistant "
-
-)
-
-# Content writer
-Content_Writer = Assistant(
-    Name = "Quantum Content Reasearcher ",
-    llm = llm,
-    Role = "Write A report on Query",
-    Tools  = []
-)
-
-# Calling alll the tolls and combine as a one agent
-Quantum_Assistant = Assistant(
-    Name  = "Quantum Marketing Assistant",
     markdown = True,
-    Teams = [Query_Summarizer,Content_Writer,Article_Reasearcher,Market_researchr,Content_Summarizer],
-    llm = llm,
+    show_tools_call = True,
 )
 
 # looping the statement
@@ -97,17 +92,3 @@ while True:
 #if the api does not functio return this message
 else :
     print("the System is not responding Please Refersh it")
-
-# To print the Memory of the LLm
-memory: AssistantMemory = Quantum_Assistant.memory
-# -*- Print Chat History
-print("============ Chat History ============")
-pprint(memory.chat_history)
-
-
-
-
-
-
-
-
